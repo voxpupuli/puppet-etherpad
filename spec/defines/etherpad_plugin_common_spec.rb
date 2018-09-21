04673d72fd2068663c28104775f67514c80b3be5
Fabien COMBERNOUS <fabien.combernous@adullact.org>
2018-09-21 21:55:48 +0200

    add test unit about nodejs::npm usage

---
+++ spec/defines/etherpad_plugin_common_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'etherpad::plugins::common' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:pre_condition) do
+        'include ::etherpad'
+      end
+      let(:facts) do
+        facts
+      end
+      let(:title) { 'ep_myplugin' }
+
+      it { is_expected.to compile.with_all_deps }
+      it {
+        is_expected.to contain_nodejs__npm('ep_myplugin').with(
+          'ensure'   => 'present',
+          'target'   => '/opt/etherpad',
+          'package'  => 'ep_myplugin',
+          'user'     => 'etherpad',
+          'home_dir' => '/opt/etherpad'
+        )
+      }
+    end
+  end
+end
