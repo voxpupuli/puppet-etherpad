7e3bf5532d349b0aaba8c104e2d27cdda73616aa
Romain Tartière <romain@blogreen.org>
2021-08-25 13:14:08 -1000

    Allow apt 8.x

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,7 +3,7 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 configure_beaker do |host|
   # Additional modules for soft deps required by puppet-nodejs
   if fact_on(host, 'os.family') == 'Debian'
-    install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.4.0 < 8.0.0')
+    install_module_from_forge_on(host, 'puppetlabs-apt', '>= 4.4.0 < 9.0.0')
   end
 
   # needed :
